77420e2e103e341c13220314335a22a25441b44d
batyrrrr <bolatbekbatyr41@gmail.com>
2023-05-01 15:51:43 +0300

    change routes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='' element={<Home />} />
+          <Route path='/newwver' element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
